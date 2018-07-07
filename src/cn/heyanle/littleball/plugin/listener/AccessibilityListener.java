41be386e01145799c265e92431410a33b909f025
何言 <1371735400@qq.com>
2018-07-07 13:01:30 +0800

    Revert "Initial commit"
    This reverts commit 20c322ed5daec9cf0d333c42a25bf07ce0337b72.

--- src/cn/heyanle/littleball/plugin/listener/AccessibilityListener.java
+++
@@ -1,7 +0,0 @@
-package cn.heyanle.littleball.plugin.listener;
-
-import android.view.accessibility.AccessibilityEvent;
-
-public interface AccessibilityListener {
-    void onAccessibilityEvent(AccessibilityEvent accessibilityEvent);
-}
